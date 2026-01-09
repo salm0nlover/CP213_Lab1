0ca9a485b86d6993119f336cb57a934159e81fc1
salm0nlover <poom_poomfast@hotmail.com>
2026-01-09 09:17:50 +0700

    worasak Change

--- GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ GreetingCard/app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             GreetingCardTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Worasak Sankla",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
